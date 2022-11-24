09a5f7ed976cd26d8bb1aabdec6ba99c14e6a00c
Tarık Talha DİNÇ <tariktalhadinc@gmail.com>
2022-11-24 11:54:02 +0300

    C# API ile CURD Operasyonları
    Personal üzerinden CRUD Operasyonları yapma

---
+++ PersonalService/PersonalInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalService
+{
+    public class Personal   
+    {
+        public int Id;
+        public string? NationalId;
+        public string? Name;
+        public string? Surname;
+        public int Age;
+
+
+        public Personal(int _Id, string _NationalId, string _Name, string _Surname, int _Age)
+        {
+            Id = _Id;
+            NationalId = _NationalId;
+            Name = _Name;
+            Surname = _Surname;
+            Age = _Age;
+        }
+
+        //public void PersoneliGoster()
+        //{
+        //    Console.WriteLine("Personel Bilgileri :" + " " + Id + " " + NationalId + " " + Name + " " + Surname + " " + Age);
+        //}
+
+    }
+}
---
+++ PersonalService/Program.cs
@@ -0,0 +1,37 @@
+﻿
+namespace PersonalService
+{
+    class PersonalService
+    {
+        static void Main()
+        {
+            //int Id = 1;
+            //string NationalId = "13595581054";
+            //string Name = "Tarık";
+            //string Surname = "Dinç";
+            //int Age = 20;
+
+
+
+            Personal personal = new(1,"13595581052","Tarık","Dinç",20);
+
+            Console.WriteLine(personal.Id);
+            Console.WriteLine(personal.NationalId);
+            Console.WriteLine(personal.Name);
+            Console.WriteLine(personal.Surname);
+            Console.WriteLine(personal.Age);
+
+
+
+
+            //personal.Id = 1;
+            //personal.NationalId = "13595581052";
+            //personal.Name = "Tarık Talha";
+            //personal.Surname = "Dinç";
+            //personal.Age = 20;
+
+
+            //personal.KisiyiGoster();
+        }
+    }
+}
